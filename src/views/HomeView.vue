a6de2085cf4668ed7747595ada696f5c64590e0f
VasaR <vasanthias52@gmail.com>
2024-10-04 21:32:33 +0530

    feat: project structure and necessary shadcn components added

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+
+const handleClick = () => {
+  console.log("clicked");
+};
+</script>
+
+<template>
+  <main>
+    <div>hello there</div>
+    <Button variant="outline" @click="handleClick"> Submit </Button>
+  </main>
+</template>
